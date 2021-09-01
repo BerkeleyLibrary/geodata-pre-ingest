ef5a622d7110803e8eb8e6b0dc48f3568f4733e7
zhouyu <zhouyu@berkeley.edu>
2021-09-01 16:17:43 -0700

    Change package name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='geodata_pre_ingest',
+    name='pre_ingestion',
     description='Package for building script tools in ArcGIS Desktop to process ESRI ISO, ISO19139,Geoblacklight metadata',
     version='1.0.0',
     author='Yuchai Zhou',
     author_email='zhouyu@berkeley.edu',
-    packages = ["geodata_pre_ingest"]
+    packages = ["pre_ingestion"]
 )
