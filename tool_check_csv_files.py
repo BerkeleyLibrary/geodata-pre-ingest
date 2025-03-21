3b3ac01a1600d25924086faf6179aa48fb2c5c3f
yzhoubk <zhouyu@berkeley.edu>
2025-03-21 14:59:49 -0700

    before add main_headers in para

---
+++ tool_check_csv_files.py
@@ -0,0 +1,57 @@
+
+import arcpy
+import common_helper
+import workspace_directory
+import check_csv_files
+import constants
+
+
+class AssignArkidTool(object):
+    def __init__(self):
+        self.label = "4 - Run verification on the main and responsible CSV files that have assigned arkids"
+
+    def getParameterInfo(self):
+        main_csv_path_param = arcpy.Parameter(
+            displayName="Main csv file",
+            name="From main csv file",
+            datatype="GPString",
+            direction="Output"
+        )    
+        responsible_csv_path_param = arcpy.Parameter(
+            displayName="Responsible csv file",
+            name="From responsible csv file",
+            datatype="GPString",
+            direction="Output"
+        )    
+        csv_path_param = arcpy.Parameter(
+            displayName="CSV files located at",
+            name="To Directory",
+            datatype="GPString",
+            direction="Output"
+
+        )
+        
+        return [ main_csv_path_param, responsible_csv_path_param, csv_path_param]
+
+    def updateParameters(self, parameters):
+        val0 = constants.no_prcess_path_selected
+        val1 = val0
+        val2 = val0
+        if (workspace_directory.source_batch_directory_path is not None):
+            csv_files_directory_path = workspace_directory.csv_files_arkid_directory_path
+            val0 = fr"{csv_files_directory_path}\main.csv"
+            val1 = fr"{csv_files_directory_path}\resp.csv"     
+        parameters[0].value = val0
+        parameters[1].value = val1
+        parameters[2].value = csv_files_directory_path
+        return
+
+    def execute(self, parameters, messages):
+        if common_helper.no_processing_directory_selected(parameters, 3):
+           return
+        if not common_helper.files_existed([parameters[0].value, parameters[1].value]):
+           return
+        
+        check_csv_files.run_tool()
+        return
+    
